39cdcb3df909109987a141ce3893f4cbe039ce62
unguest <unguestdev@gmail.com>
2020-08-30 12:08:08 +0200

    TYPO + GH problem resolved...

--- Pybonnacci.py
+++ Pybonnacci.py
@@ -17,10 +17,7 @@ if __name__ == "__main__":
         print(fib(i))
     stop = time.time()
 
-<<<<<<< HEAD
     print(f'Time to compute the first thousand of Fibonacci\'s sequence : {stop - start} seconds')
-
-
 
 """
 To compute the nTh term of the fibonacci sequence, we use the Binet's theorem.
@@ -30,6 +27,3 @@ fib(n) =  ((____________)^n) / sqrt(5)
 	       	 2
 
 """
-=======
-    print(f'Time to compute the first thousand of Fibonacci\'s sequence : {stop - start} seconds')
->>>>>>> ce62f9db671e5996f87dcbd83ff31c79124a0237
